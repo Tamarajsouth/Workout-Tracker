3b036cd4e5072216514bebefc1afb1f906ba7039
tamara south <tjeansouth@gmail.com>
2020-06-11 18:34:11 -0400

    added some clarifying comments to my code for my understanding and updated README.md

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,7 @@
+// The path module provides utilities for working with file and directory paths
 const path = require("path");
 
+// routing using express
 module.exports = function (app) {
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+// Request logger middleware for node.js
 const morgan = require("morgan");
+// Mongoose uses Schemas to map to MongoDB Collection
 const mongoose = require("mongoose");
 const app = express();
 
@@ -7,8 +9,10 @@ const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
 app.use(morgan("dev"));
+// Method in express to recognize the incoming Request Object as strings or arrays
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// To serve static files in express apps
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
